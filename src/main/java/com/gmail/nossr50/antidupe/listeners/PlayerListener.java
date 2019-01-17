5a7df866f4e331c29d58f3b8abdd289bd16cb89e
nossr50 <nossr50@gmail.com>
2019-01-17 13:55:00 -0800

    Find out which plugins use this event and blame them

--- src/main/java/com/gmail/nossr50/antidupe/listeners/PlayerListener.java
+++ src/main/java/com/gmail/nossr50/antidupe/listeners/PlayerListener.java
@@ -5,9 +5,13 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.plugin.RegisteredListener;
+
+import java.util.ArrayList;
 
 /**
  * This plugin was made for floris to solve a problem on his server involving duping by dropping items off boats
@@ -15,6 +19,11 @@ import org.bukkit.event.player.PlayerDropItemEvent;
 
 public class PlayerListener implements Listener {
     private AntiDupe antiDupe;
+
+    private boolean entityPluginsRecorded = false;
+    private boolean playerDropPluginsRecorded = false;
+
+    ArrayList<String> pluginsUsingThisEvent = new ArrayList<>();
 
 
     public PlayerListener(AntiDupe antiDupe)
@@ -25,6 +34,13 @@ public class PlayerListener implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onEntityDropItemEvent(EntityDropItemEvent event)
     {
+        if(entityPluginsRecorded == false)
+        {
+            blamePlugins(event.getHandlers());
+
+            entityPluginsRecorded = true;
+        }
+
         if(event.isCancelled())
             return;
 
@@ -39,17 +55,16 @@ public class PlayerListener implements Listener {
         }
     }
 
-    private void alertAdmins(Player player) {
-        for (Player admin : antiDupe.getServer().getOnlinePlayers()) {
-            if (admin.isOp()) {
-                admin.sendMessage(ChatColor.RED+"[nossr50 exploit alert] " +ChatColor.YELLOW+ player.getName() +ChatColor.WHITE+ " was trying to drop an item from a vehicle!");
-            }
-        }
-    }
-
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerDropItemEvent(PlayerDropItemEvent event)
     {
+        if(playerDropPluginsRecorded == false)
+        {
+            blamePlugins(event.getHandlers());
+
+            playerDropPluginsRecorded = true;
+        }
+
         if(event.isCancelled())
             return;
 
@@ -58,6 +73,27 @@ public class PlayerListener implements Listener {
         {
             event.setCancelled(true);
             alertAdmins(player);
+        }
+    }
+
+    private void blamePlugins(HandlerList handlers) {
+        for (RegisteredListener otherPlugins : handlers.getRegisteredListeners()) {
+            pluginsUsingThisEvent.add(otherPlugins.getPlugin().getName());
+        }
+
+        System.out.print("[nossr50 BEGIN ANTI DUPE PLUGIN BLAME REPORT]");
+        for (String s : pluginsUsingThisEvent) {
+
+            System.out.println(s + " is interacting with the EntityDropItemEvent!");
+        }
+        System.out.print("[nossr50 END ANTI DUPE PLUGIN BLAME REPORT]");
+    }
+
+    private void alertAdmins(Player player) {
+        for (Player admin : antiDupe.getServer().getOnlinePlayers()) {
+            if (admin.isOp()) {
+                admin.sendMessage(ChatColor.RED+"[nossr50 exploit alert] " +ChatColor.YELLOW+ player.getName() +ChatColor.WHITE+ " was trying to drop an item from a vehicle!");
+            }
         }
     }
 }
